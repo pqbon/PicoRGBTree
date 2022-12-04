c10a0099b2fe05277f8eb94ac826af3af599728e
PQBON <62471694+pqbon@users.noreply.github.com>
2022-12-04 23:15:48 +0000

    Update main.py for infinate repeat...

--- main.py
+++ main.py
@@ -2,12 +2,15 @@ from xmas_tree_rg import xmas_tree_redgreen
 from xmas_tree_colours import xmas_tree_colours 
 from xmas_tree_pastel import xmas_tree_pastel 
 from xmas_tree_solid import xmas_tree_solid 
-from xmas_tree_waves import xmas_tree_waves 
+from xmas_tree_waves import xmas_tree_waves
+from xmas_tree_random import xmas_tree_random 
 
 
 if __name__ == '__main__':
-    xmas_tree_redgreen(100)
-    xmas_tree_colours(100)
-    xmas_tree_pastel(100)
-    xmas_tree_solid(100)
-    xmas_tree_waves(100)
+    while _ :
+        xmas_tree_redgreen(100)
+        xmas_tree_colours(100)
+        xmas_tree_pastel(100)
+        xmas_tree_solid(100)
+        xmas_tree_waves(100)
+        xmas_tree_random(10000)
